cfe5a5f80ffba9a89854ea06c43634c26ebe55f4
Brian Russell <Brianrussell2017@yahoo.com>
2024-11-21 16:19:10 -0500

    Add project files.

---
+++ DataSource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinformsAssignment3_3
+{
+    public class DataSource
+    {
+        public static List<Student> Students;
+
+
+        public static void CreateData()
+        {
+            Students = new List<Student>()
+            { new Student() { StudentId =1, FirstName="Brian", LastName="Russell", Address="123 Nottrue St."},
+             new Student() { StudentId =2, FirstName="Joe", LastName="Joester", Address="124 Nottrue Ave." },
+             new Student() { StudentId =1, FirstName="Jane", LastName="Janeison", Address="125 Nottrue Blvd." }
+            };
+
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinformsAssignment3_3
+{
+    public class Student
+    {
+        public enum MonthofAdmission
+        {
+            January,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December
+        }
+
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+        public char Grade { get; set; }
+
+        public MonthofAdmission MonthOfAdmision { get; set; }
+
+
+        public static void AddStudent(Student student)
+        {
+            List<Student> Students = new List<Student>();
+            if (student != null)
+            {
+
+                Students.Add(new Student() { StudentId = 0, FirstName = " ", LastName = " ", Address = " " });
+            }
+            else
+            {
+                Console.WriteLine("Cannot add student");
+
+
+            }
+
+
+        }
+    }
+}
